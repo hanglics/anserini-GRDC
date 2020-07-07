85a199573750a5a877c29a3a6e978a3c1f951d7f
Hang Li <cecillll.lee@gmail.com>
2020-07-07 13:34:49 +1000

    add grdc collection code

--- src/main/java/io/anserini/collection/GRDCCollection.java
+++ src/main/java/io/anserini/collection/GRDCCollection.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.lang.StringBuilder;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Arrays;
@@ -17,7 +16,7 @@ import java.util.NoSuchElementException;
 public class GRDCCollection extends DocumentCollection<GRDCCollection.Document> {
     public GRDCCollection(Path path) {
         this.path = path;
-        this.allowedFileSuffix = new HashSet<>(Arrays.asList(".json", ".jsonl"));
+        this.allowedFileSuffix = new HashSet<>(Arrays.asList(".json"));
     }
 
     @Override
@@ -66,152 +65,128 @@ public class GRDCCollection extends DocumentCollection<GRDCCollection.Document> 
         }
     }
 
+    protected static class attachment {
+        protected String report_id;
+        protected String attachment_url;
+        protected String attachment_id;
+        protected String attachment_name;
+        protected String attachment_size;
+        protected String attachment_type;
+        protected String attachment_base64_content;
+        protected String attachment_full_text_content;
+    }
+
     public static class Document implements SourceDocument {
-        protected String id;
-        protected String title;
-        protected String abstractContent;
+        protected String report_id;
+        protected String project_number;
+        protected String report_title;
+        protected String region_name;
+        protected String category_name;
+        protected String research_theme_name;
+        protected String organization_name;
+        protected String commence_date;
+        protected String complete_date;
+        protected String state;
+        protected String supervisor_name;
+        protected String report_type;
+        protected String report_status;
+        protected String publish_date;
+        protected String report_summary;
+        protected String[] keywords;
+        protected String pdf_url;
+        protected String web_url;
+        protected String html_content;
+        protected String report_achievement;
+        protected String report_conclusion;
+        protected String report_outcome;
+        protected String report_recommendation;
+        protected String report_discussion;
+        protected String other_research;
+        protected String ip_summary;
+        protected String additional_information;
+        protected String report_full_text_content;
+        protected attachment[] attachments;
         protected String raw;
-        protected String organisation;
-        protected String[] responsibleParty;
-        protected String catalogue;
-        protected String publish_time;
-        protected String url;
-        protected double[] latitude;
-        protected double[] longitude;
-        protected String coordinates;
 
         public Document(JsonNode json) {
-            // extracting the fields from the ISO19115 file
+            // extracting the fields from the GRDC json file
             this.raw = json.toString();
-            String identifier = json.get("gmd:MD_Metadata").get("gmd:fileIdentifier").get("gco:CharacterString")
-                    .asText();
-            // extracting the id in the beginning of the text
-            this.id = identifier.substring(0, identifier.length() - 8);
-            this.title = json.get("gmd:MD_Metadata").get("gmd:identificationInfo").get("gmd:MD_DataIdentification")
-                    .get("gmd:citation").get("gmd:CI_Citation").get("gmd:title").get("gco:CharacterString").asText();
-            this.abstractContent = json.get("gmd:MD_Metadata").get("gmd:identificationInfo")
-                    .get("gmd:MD_DataIdentification").get("gmd:abstract").get("gco:CharacterString").asText();
-            this.organisation = json.get("gmd:MD_Metadata").get("gmd:contact").get("gmd:CI_ResponsibleParty")
-                    .get("gmd:organisationName").get("gco:CharacterString").asText();
-            this.catalogue = json.get("gmd:MD_Metadata").get("gmd:contact").get("gmd:CI_ResponsibleParty")
-                    .get("gmd:individualName").get("gco:CharacterString").asText();
-            this.publish_time = json.get("gmd:MD_Metadata").get("gmd:dateStamp").get("gco:Date").asText();
-            this.url = json.get("gmd:MD_Metadata").get("gmd:dataSetURI").get("gco:CharacterString").asText();
 
-            // extracting all the authors of the paper
-            JsonNode parties_node = json.get("gmd:MD_Metadata").get("gmd:identificationInfo")
-                    .get("gmd:MD_DataIdentification").get("gmd:citation").get("gmd:CI_Citation")
-                    .get("gmd:citedResponsibleParty");
-            // extracting individual authors from the ResponsibleParty field
-            int number_of_parties = parties_node.size();
-            responsibleParty = new String[number_of_parties];
-            for (int i = 0; i < number_of_parties; i++) {
-                responsibleParty[i] = parties_node.get(i).get("gmd:CI_ResponsibleParty").get("gmd:individualName")
-                        .get("gco:CharacterString").asText();
-            }
+            this.report_id = json.get("report_id").asText();
+            this.project_number = json.get("project_number").asText();
+            this.report_title = json.get("report_title").asText();
+            this.region_name = json.get("region_name").asText();
+            this.category_name = json.get("category_name").asText();
+            this.research_theme_name = json.get("research_theme_name").asText();
+            this.organization_name = json.get("organization_name").asText();
+            this.commence_date = json.get("commence_date").asText();
+            this.complete_date = json.get("complete_date").asText();
+            this.state = json.get("state").asText();
+            this.supervisor_name = json.get("supervisor_name").asText();
+            this.report_type = json.get("report_type").asText();
+            this.report_status = json.get("report_status").asText();
+            this.publish_date = json.get("publish_date").asText();
+            this.report_summary = json.get("report_summary").asText();
+            this.pdf_url = json.get("pdf_url").asText();
+            this.web_url = json.get("web_url").asText();
+            this.html_content = json.get("html_content").asText();
+            this.report_achievement = json.get("report_achievement").asText();
+            this.report_conclusion = json.get("report_conclusion").asText();
+            this.report_outcome = json.get("report_outcome").asText();
+            this.report_recommendation = json.get("report_recommendation").asText();
+            this.report_discussion = json.get("report_discussion").asText();
+            this.other_research = json.get("other_research").asText();
+            this.ip_summary = json.get("ip_summary").asText();
+            this.additional_information = json.get("additional_information").asText();
+            this.report_full_text_content = json.get("report_full_text_content").asText();
 
-            // extracting the latitudes from the paper, 5 points as the polygon needs to be
-            // enclosed
-            latitude = new double[4];
-            latitude[0] = json.get("gmd:MD_Metadata").get("gmd:identificationInfo").get("gmd:MD_DataIdentification")
-                    .get("gmd:extent").get("gmd:EX_Extent").get("gmd:geographicElement")
-                    .get("gmd:EX_GeographicBoundingBox").get("gmd:northBoundLatitude").get("gco:Decimal").asDouble();
-            latitude[2] = json.get("gmd:MD_Metadata").get("gmd:identificationInfo").get("gmd:MD_DataIdentification")
-                    .get("gmd:extent").get("gmd:EX_Extent").get("gmd:geographicElement")
-                    .get("gmd:EX_GeographicBoundingBox").get("gmd:southBoundLatitude").get("gco:Decimal").asDouble();
-            // ensuring that a single coordinate location will be drawn as a small rectangle
-            if (latitude[0] == latitude[2]) {
-                latitude[0] -= 0.00001;
-                latitude[2] += 0.00001;
-            }
-            latitude[1] = latitude[0];
-            latitude[3] = latitude[2];
+            // get all keywords as JsonNode
+            JsonNode keywords_node = json.get("keywords");
 
-            // extracting the longitudes from the paper, again 5 points are needed to
-            // enclose the polygon
-            longitude = new double[4];
-            longitude[0] = json.get("gmd:MD_Metadata").get("gmd:identificationInfo").get("gmd:MD_DataIdentification")
-                    .get("gmd:extent").get("gmd:EX_Extent").get("gmd:geographicElement")
-                    .get("gmd:EX_GeographicBoundingBox").get("gmd:westBoundLongitude").get("gco:Decimal").asDouble();
-            longitude[1] = json.get("gmd:MD_Metadata").get("gmd:identificationInfo").get("gmd:MD_DataIdentification")
-                    .get("gmd:extent").get("gmd:EX_Extent").get("gmd:geographicElement")
-                    .get("gmd:EX_GeographicBoundingBox").get("gmd:eastBoundLongitude").get("gco:Decimal").asDouble();
-            // ensuring that a single coordinate location will be drawn as a small rectangle
-            if (longitude[0] == longitude[1]) {
-                longitude[0] -= 0.00001;
-                longitude[1] += 0.00001;
-            }
-            longitude[2] = longitude[1];
-            longitude[3] = longitude[0];
+            // extracting all keywords from keywords field
+            int number_of_keywords = keywords_node.size();
+            this.keywords = new String[number_of_keywords];
 
-            this.coordinates = getCoordinateString();
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public String getAbstract() {
-            return abstractContent;
-        }
-
-        public String getOrganisation() {
-            return organisation;
-        }
-
-        public String[] getResponsibleParty() {
-            return responsibleParty;
-        }
-
-        public String getCatalogue() {
-            return catalogue;
-        }
-
-        public String getPublish_time() {
-            return publish_time;
-        }
-
-        public String getUrl() {
-            return url;
-        }
-
-        public String getCoordinates() {
-            return coordinates;
-        }
-
-        private String getCoordinateString() {
-            StringBuilder coordinates = new StringBuilder("[");
-            // generating it in this form for literal evaluation in javascript
-            for (int i = 0; i < 4; i++) {
-                coordinates.append("[");
-                coordinates.append(latitude[i]);
-                coordinates.append(",");
-                coordinates.append(longitude[i]);
-                coordinates.append("]");
-                if (i != 3) {
-                    coordinates.append(",");
+            // check if there are keywords, if not, assign empty array
+            if (number_of_keywords > 0) {
+                for (int i = 0; i < number_of_keywords; i++) {
+                    this.keywords[i] = keywords_node.get(i).asText();
                 }
             }
-            coordinates.append("]");
-            return coordinates.toString();
-        }
 
-        public double[] getLatitude() {
-            return latitude;
-        }
+            // get all attachments as JsonNode
+            JsonNode attachments_node = json.get("attachments");
 
-        public double[] getLongitude() {
-            return longitude;
+            // extracting all attachments from the attachments field
+            int number_of_attachments = attachments_node.size();
+            this.attachments = new attachment[number_of_attachments];
+
+            // check if there are attachments, if not, assign empty array
+            if (number_of_attachments > 0) {
+                // extracting individual attachment from all attachments
+                for (int i = 0; i < number_of_attachments; i++) {
+                    this.attachments[i].attachment_base64_content = attachments_node.get(i)
+                            .get("attachment_base64_content").asText();
+                    this.attachments[i].attachment_full_text_content = attachments_node.get(i)
+                            .get("attachment_full_text_content").asText();
+                    this.attachments[i].attachment_id = attachments_node.get(i).get("attachment_id").asText();
+                    this.attachments[i].attachment_name = attachments_node.get(i).get("attachment_name").asText();
+                    this.attachments[i].attachment_size = attachments_node.get(i).get("attachment_size").asText();
+                    this.attachments[i].attachment_type = attachments_node.get(i).get("attachment_type").asText();
+                    this.attachments[i].attachment_url = attachments_node.get(i).get("attachment_url").asText();
+                    this.attachments[i].report_id = attachments_node.get(i).get("report_id").asText();
+                }
+            }
         }
 
         @Override
         public String id() {
-            return id;
+            return report_id;
         }
 
         @Override
         public String contents() {
-            return title + "\n" + abstractContent;
+            return report_full_text_content;
         }
 
         @Override
@@ -222,6 +197,114 @@ public class GRDCCollection extends DocumentCollection<GRDCCollection.Document> 
         @Override
         public boolean indexable() {
             return true;
+        }
+
+        public String getProjectNumber() {
+            return project_number;
+        }
+
+        public String getReportTitle() {
+            return report_title;
+        }
+
+        public String getRegionName() {
+            return region_name;
+        }
+
+        public String getCategoryName() {
+            return category_name;
+        }
+
+        public String getResearchThemeName() {
+            return research_theme_name;
+        }
+
+        public String getOrganizationName() {
+            return organization_name;
+        }
+
+        public String getCommenceDate() {
+            return commence_date;
+        }
+
+        public String getCompleteDate() {
+            return complete_date;
+        }
+
+        public String getState() {
+            return state;
+        }
+
+        public String getSupervisorName() {
+            return supervisor_name;
+        }
+
+        public String getReportType() {
+            return report_type;
+        }
+
+        public String getReportStatus() {
+            return report_status;
+        }
+
+        public String getPublishDate() {
+            return publish_date;
+        }
+
+        public String getReportSummary() {
+            return report_summary;
+        }
+
+        public String[] getKeywords() {
+            return keywords;
+        }
+
+        public String getPDFURL() {
+            return pdf_url;
+        }
+
+        public String getWebURL() {
+            return web_url;
+        }
+
+        public String getHTMLContent() {
+            return html_content;
+        }
+
+        public String getReportAchievement() {
+            return report_achievement;
+        }
+
+        public String getReportConclusion() {
+            return report_conclusion;
+        }
+
+        public String getReportOutcome() {
+            return report_outcome;
+        }
+
+        public String getReportRecommendation() {
+            return report_recommendation;
+        }
+
+        public String getReportDiscussion() {
+            return report_discussion;
+        }
+
+        public String getOtherResearch() {
+            return other_research;
+        }
+
+        public String getIPSummary() {
+            return ip_summary;
+        }
+
+        public String getAdditionalInformation() {
+            return additional_information;
+        }
+
+        public attachment[] getAttachments() {
+            return attachments;
         }
 
     }
--- src/main/java/io/anserini/index/generator/GRDCGenerator.java
+++ src/main/java/io/anserini/index/generator/GRDCGenerator.java
@@ -9,9 +9,20 @@ public class GRDCGenerator extends DefaultLuceneDocumentGenerator<GRDCCollection
 
     // constants for storing
     public enum GRDCFields {
-        ID("id"), TITLE("title"), ABSTRACT("abstract"), ORGANISATION("organisation"),
-        RESPONSIBLE_PARTY("responsible_party"), CATALOGUE("catalogue"), PUBLISH_TIME("publish_time"), URL("url"),
-        COORDINATES("coordinates");
+        REPORT_ID("report_id"), PROJECT_NUMBER("project_number"), REPORT_TITLE("report_title"),
+        REGION_NAME("region_name"), CATEGORY_NAME("category_name"), RESEARCH_THEME_NAME("research_theme_name"),
+        ORGANIZATION_NAME("organization_name"), COMMENCE_DATE("commence_date"), COMPLETE_DATE("complete_date"),
+        STATE("state"), SUPERVISOR_NAME("supervisor_name"), REPORT_TYPE("report_type"), REPORT_STATUS("report_status"),
+        PUBLISH_DATE("publish_date"), REPORT_SUMMARY("report_summary"), KEYWORDS("keywords"), PDF_URL("pdf_url"),
+        WEB_URL("web_url"), HTML_CONTENT("html_content"), REPORT_ACHIEVEMENT("report_achievement"),
+        REPORT_CONCLUSION("report_conclusion"), REPORT_OUTCOME("report_outcome"),
+        REPORT_RECOMMENDATION("report_recommendation"), REPORT_DISCUSSION("report_discussion"),
+        OTHER_RESEARCH("other_research"), IP_SUMMARY("ip_summary"), ADDITIONAL_INFORMATION("additional_information"),
+        REPORT_FULL_TEXT_CONTENT("report_full_text_content"), ATTACHMENTS("attachments"),
+        ATTACHMENT_URL("attachment_url"), ATTACHMENT_ID("attachment_id"), ATTACHMENT_NAME("attachment_name"),
+        ATTACHMENT_SIZE("attachment_size"), ATTACHMENT_TYPE("attachment_type"),
+        ATTACHMENT_BASE64_CONTENT("attachment_base64_content"),
+        ATTACHMENT_FULL_TEXT_CONTENT("attachment_full_text_content"), RAW("raw");
 
         public final String name;
 
@@ -27,6 +38,8 @@ public class GRDCGenerator extends DefaultLuceneDocumentGenerator<GRDCCollection
 
     public Document createDocument(GRDCCollection.Document doc) throws GeneratorException {
         Document document = super.createDocument(doc);
+
+        document.add(new StoredField(GRDCFields.REPORT_ID.name, doc.id()));
 
         document.add(new StoredField(GRDCFields.TITLE.name, doc.getTitle()));
         document.add(new StoredField(GRDCFields.ABSTRACT.name, doc.getAbstract()));
